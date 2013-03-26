244c212af080a0f296121e8b6e8c9f5d20f7cd34
jlyons871 <jakelyons87@gmail.com>
2013-03-26 13:58:16 -0400

    added support to recursively enter all folders and preprocess email files

--- Jake/preinterns.py
+++ Jake/preinterns.py
@@ -4,26 +4,12 @@
 
 
 import re 
-## import regular expressions lib
-## forget which of these libs come with python by default
-## very easy to install new libs using pip command from cmd prompt, just google it
 import pprint 
-import os.path
-## use import in similar manner to import another entire python file or functions
-## see python_intro powerpoint
+import os
 
-
-## on my comp have files in folder, at same level as this file, named 'testdata'
-## inside folder has files, no extension ranging from 1 to 898
-## 'fname_in' is path for testing data... '\' is escape character
-fname_in = "testdata\\"
-## prob is way to determine range automatically
-my_range = 898
-## append number to path above in loop below
-## later easy to expand to allow for cmd line inputs
-
+# fname_in = "testdata\\"
+# my_range = 898
 fname_out = "outdata\\"
-## manually created empty folder to store output files
 
 ## check if file exists
 def file_exists(filename):
@@ -40,36 +26,84 @@ def get_email_body(filename):
         
     f = open(filename, 'r')
     body_of_email = False   
+    forward_info = False
     body_regex = r'X-FileName:'
+    forward_regex = r'-------- Forwarded'
+    endHeader_regex = r'Subject:'
     
-    ## body_data = []
-    ## storing in an array(or list) would be faster since strings are immutable
-    ## but its easier for now to append to a str
     body_data = ""  
    
     for line in f: 
+        ## set flag for start of forward header data
+        if re.search(forward_regex, line) != None:
+            forward_info = True  
+
+        ## set 'body_of_email' true after header section
         if not body_of_email:
             if re.search(body_regex, line) != None:
-                body_of_email = True
-        else:
-            #body_data.append(line)  ##to store in array
+                body_of_email = True  
+                
+        ## set flag for end of forward header data
+        elif forward_info:
+            if re.search(endHeader_regex, line) != None:
+                forward_info = False            
+                
+        ## only copy body of email
+        elif body_of_email and not forward_info:
             body_data += line
+
             
     f.close()
     return body_data
 
+all_files = []    
     
-## create a list of filenames ranging from 0 to myrange   
-files = [i for i in range(1,my_range)]
-errors = []
-for filenum in files:
+for dirname, dirnames, filenames in os.walk('.'):
+    # print path to all subdirectories first.
+    # for subdirname in dirnames:
+        # print os.path.join(dirname, subdirname)         
+
+    # Advanced usage:
+    # editing the 'dirnames' list will stop os.walk() from recursing into there.
+    if '.git' in dirnames:
+        # don't go into any .git directories.
+        dirnames.remove('.git')    
+    
+    # print path to all filenames.
+    for filename in filenames:
+        if ('.py' not in filename) and ('.txt' not in filename):
+            all_files.append(os.path.join(dirname, filename))
+            #print os.path.join(dirname, filename)    
+
+count = 1    
+errors = []        
+for file in all_files:
     try:
-        data = get_email_body(fname_in + str(filenum))    
-        f_out = open(fname_out + str(filenum) + '.txt', 'w')
-        f_out.write('\n\nEmail Message:\n\n' + data)
+        data = get_email_body(file)    
+        f_out = open(fname_out + str(count) + '.txt', 'w')
+        f_out.write('\n\nEmail Message:\n\n' + data + '\n\nFileName' + file)
         f_out.close()
     except Exception as error:
-        pass
+        errors.append(file)
+    count += 1
+
+    
+    
+    
+### old way     
+    
+## create a list of filenames ranging from 0 to myrange   
+# files = [i for i in range(1,my_range)]
+# errors = []
+# for filenum in files:
+    # try:
+        # data = get_email_body(fname_in + str(filenum))    
+        # f_out = open(fname_out + str(filenum) + '.txt', 'w')
+        # f_out.write('\n\nEmail Message:\n\n' + data)
+        # f_out.close()
+    # except Exception as error:
+        # pass
+        
         # errors.append("couldnt open: " + str(filenum))
         # 'file_exists' funct will print out bad file names
         
